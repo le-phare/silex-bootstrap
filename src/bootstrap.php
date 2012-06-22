e50df477cf572c762e5cc3647b324e79e4018ba9
Erwan Richard <erwann.richard@gmail.com>
2012-06-22 10:47:23 +0200

    Remove doctrine extension

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,8 +3,6 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\DoctrineServiceProvider;
-use Repository\DumbRepository;
 
 $app = new Silex\Application();
 
@@ -14,21 +12,5 @@ $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/templates',
     'twig.options' => array('cache' => __DIR__.'/../cache'),
 ));
-
-$app->register(new DoctrineServiceProvider);
-
-$app['db.options'] = array(
-    'driver'   => 'pdo_mysql',
-    'dbname'   => 'dumb',
-    'host'     => 'localhost',
-    'user'     => 'root',
-    'password' => 'root',
-);
-
-$app->before(function() use ($app) {
-    $app['db.dumb'] = $app->share(function($app) {
-        return new DumbRepository($app['db']);
-    });
-});
 
 return $app;
