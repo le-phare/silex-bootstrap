44cb0a0fba48523929d7a688076bbdb6cb0ce21a
Erwan Richard <erwann.richard@gmail.com>
2012-09-03 11:20:57 +0200

    Add locale in routes

--- src/app.php
+++ src/app.php
@@ -4,12 +4,14 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app = require __DIR__.'/bootstrap.php';
 
-$app->get('/', function() use ($app){
-    return $app['twig']->render('home.html.twig');
-});
+$app->get('/{locale}', function($locale) use ($app) {
+    $app['translator']->setLocale($locale);
+    return $app['twig']->render('home.html.twig', array( 'locale' => $locale ));
+})->value('locale', 'fr');;
 
-$app->get('/{controller}/{action}', function($controller,$action) use ($app){
-    return $app['twig']->render($controller.'/'.$action.'.html.twig');
+$app->get('/{locale}/{controller}/{action}', function($locale,$controller,$action) use ($app){
+    $app['translator']->setLocale($locale);
+    return $app['twig']->render($controller.'/'.$action.'.html.twig', array( 'locale' => $locale ));
 });
 
 $app->error(function (NotFoundHttpException $e, $code) use ($app){
