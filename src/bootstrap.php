eb8b492cf59c0fa336d5ff777b9abfbff00c8c34
Erwan Richard <erwann.richard@gmail.com>
2012-09-03 11:22:28 +0200

    Add Assetic dependency.
    Less filter is enabled with the LessPHP compiler.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,17 +5,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
+use SilexExtension\AsseticExtension;
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
+//
+// Twig
+//
 $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/templates',
-    'twig.options' => array('cache' => __DIR__.'/../cache'),
+    'twig.options' => array('cache' => false),
 ));
 
+//
+// Translator
+//
 $app->register(new TranslationServiceProvider(), array(
+    'locale'          => 'fr',
     'locale_fallback' => 'fr',
 ));
 
@@ -36,5 +44,20 @@ $app['translator'] = $app->share($app->extend('translator', function($translator
 
     return $translator;
 }));
+
+//
+// Assetic
+//
+$app->register(new AsseticExtension(), array(
+    'assetic.class_path' => __DIR__.'/vendor/assetic/src',
+    'assetic.path_to_web' => __DIR__ . '/../web',
+    'assetic.options' => array(
+        'debug' => $app['debug'],
+        'auto_dump_assets' => true,
+    ),
+    'assetic.filters' => $app->protect(function($fm) {
+        $fm->set('less', new Assetic\Filter\LessphpFilter());
+    })
+));
 
 return $app;
