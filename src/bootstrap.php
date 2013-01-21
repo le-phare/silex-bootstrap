7b866c1a33d721792707969d8b1b1746bc135023
Erwan Richard <erwann.richard@gmail.com>
2013-01-21 16:45:58 +0100

    Fix SilexAssetic extension namespace

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
-use SilexExtension\AsseticExtension;
+use SilexAssetic\AsseticExtension;
 
 $app = new Silex\Application();
 
