027fe71f7116a24202729028b58e43aab01dc201
Erwan Richard <erwann.richard@gmail.com>
2013-01-29 12:26:48 +0100

    Allow template to be store anywhere

--- src/app.php
+++ src/app.php
@@ -12,7 +12,7 @@ $app->get('/{locale}', function($locale) use ($app) {
 $app->get('/{locale}/{controller}/{action}', function($locale,$controller,$action) use ($app){
     $app['translator']->setLocale($locale);
     return $app['twig']->render($controller.'/'.$action.'.html.twig', array( 'locale' => $locale ));
-});
+})->assert('action', '.+');
 
 $app->error(function (NotFoundHttpException $e, $code) use ($app){
     return $app['twig']->render('error/404.html.twig');
